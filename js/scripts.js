27966d553e0a1cb03817b65129530e3b9a34f80f
Stellr <blakeowest@gmail.com>
2021-04-07 11:11:59 -0500

    Form Updates

--- js/scripts.js
+++ js/scripts.js
@@ -115,3 +115,26 @@ darkModeToggle.addEventListener('click', () => {
         disableDarkMode();
     }
 });
+
+const processForm = form => {
+  const data = new FormData(form)
+  data.append('form-name', 'newsletter');
+  fetch('/', {
+    method: 'POST',
+    body: data,
+  })
+  .then(() => {
+    form.innerHTML = `<div class="form--success">Almost there! Check your inbox for a confirmation e-mail.</div>`;
+  })
+  .catch(error => {
+    form.innerHTML = `<div class="form--error">Error: ${error}</div>`;
+  })
+}
+
+const emailForm = document.querySelector('.email-form')
+if (emailForm) {
+  emailForm.addEventListener('submit', e => {
+    e.preventDefault();
+    processForm(emailForm);
+  })
+}
